7281cfe9830f965618c881891f84e7d58309e9f1
j-markiewicz <j.markiewicz@student.uj.edu.pl>
2022-12-02 06:23:12 +0100

    refactor day 1

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,13 +1,14 @@
-const INPUT: &str = include_str!("input.txt");
+use std::fs;
 
 fn main() {
-    let food: Vec<&str> = INPUT.split("\n\n").collect();
-    let mut food: Vec<u64> = food
-        .into_iter()
-        .map(|s| s.lines().map(|n| n.parse::<u64>().unwrap()).sum())
-        .collect();
-    food.sort_by(|a, b| b.cmp(a));
+	let mut food: Vec<u64> = fs::read_to_string("./input.txt")
+		.unwrap()
+		.split("\n\n")
+		.map(|s| s.lines().map(|n| n.parse::<u64>().unwrap()).sum())
+		.collect();
 
-    println!("1: {}", food[0]);
-    println!("2: {}", food[0..=2].iter().sum::<u64>())
+	food.sort_by(|a, b| b.cmp(a));
+
+	println!("1: {}", food[0]);
+	println!("2: {}", food[0..=2].iter().sum::<u64>())
 }
