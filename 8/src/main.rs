b1798ad791787f4b1064433d72cc3b9d97488a69
Jan Markiewicz <j.markiewicz@student.uj.edu.pl>
2022-12-08 08:58:02 +0100

    add day 8

---
+++ 8/src/main.rs
@@ -0,0 +1,89 @@
+use std::{error::Error, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("./input.txt")?;
+
+    let trees = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_string().parse::<i8>())
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut n_visible = 0;
+
+    for i in 0..trees.len() {
+        for j in 0..trees[0].len() {
+            if trees[i][j] > *trees[i][j + 1..].iter().max().unwrap_or(&i8::MIN) {
+                n_visible += 1;
+            } else if trees[i][j] > *trees[i][..j].iter().max().unwrap_or(&i8::MIN) {
+                n_visible += 1;
+            } else if trees[i][j] > trees[i + 1..].iter().map(|t| t[j]).max().unwrap_or(i8::MIN) {
+                n_visible += 1;
+            } else if trees[i][j] > trees[..i].iter().map(|t| t[j]).max().unwrap_or(i8::MIN) {
+                n_visible += 1;
+            }
+        }
+    }
+
+    println!("1: {n_visible}");
+
+    let mut scores = vec![vec![0u64; trees[0].len()]; trees.len()];
+
+    for i in 0..trees.len() {
+        for j in 0..trees[0].len() {
+            let mut score = 1;
+
+            let mut s = 0;
+            for tree in trees[i][j + 1..].iter().copied() {
+                s += 1;
+
+                if tree >= trees[i][j] {
+                    break;
+                }
+            }
+            score *= s;
+
+            let mut s = 0;
+            for tree in trees[i][..j].iter().rev().copied() {
+                s += 1;
+
+                if tree >= trees[i][j] {
+                    break;
+                }
+            }
+            score *= s;
+
+            let mut s = 0;
+            for tree in trees[i + 1..].iter().map(|t| t[j]) {
+                s += 1;
+
+                if tree >= trees[i][j] {
+                    break;
+                }
+            }
+            score *= s;
+
+            let mut s = 0;
+            for tree in trees[..i].iter().rev().map(|t| t[j]) {
+                s += 1;
+
+                if tree >= trees[i][j] {
+                    break;
+                }
+            }
+            score *= s;
+
+            scores[i][j] = score;
+        }
+    }
+
+    println!(
+        "2: {}",
+        scores.into_iter().flatten().max().unwrap_or(u64::MAX)
+    );
+
+    Ok(())
+}
